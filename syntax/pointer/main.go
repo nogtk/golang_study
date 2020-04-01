89103d1b49527754781b3e4466d32b0695ef8e6f
nogtk <taka.naoga@moneyforward.co.jp>
2020-04-01 21:40:54 +0900

    Check addr

--- syntax/pointer/main.go
+++ syntax/pointer/main.go
@@ -8,9 +8,11 @@ func main() {
 	n := 100
 	// 値渡し
 	// コピーされるので、元のnに変化はない
+	fmt.Printf("n(main) addr is %p\n", &n)
 	returnValue := increment(n)
 	fmt.Printf("Value of n is %d\n", n)
 	fmt.Printf("Return Value of increment is %d\n", returnValue)
+
 	// 参照渡し
 	// 戻り値を受け取らなくても渡した変数が書き換わる
 	incrementWithPointer(&n)
@@ -18,8 +20,10 @@ func main() {
 }
 
 func increment(n int) int {
+	fmt.Printf("n(increment) addr is %p\n", &n)
 	return n + 1
 }
 func incrementWithPointer(n *int) {
+	fmt.Printf("n(incrementWithPointer) addr is %p\n", n)
 	*n++
 }
